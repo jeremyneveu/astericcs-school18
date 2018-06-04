73a89619107bec7626f56bc15886d993311aac75
Jérémy Neveu <jneveu@lal.in2p3.fr>
2018-06-04 11:42:49 +0200

    first working

--- fib.py
+++ fib.py
@@ -1,3 +1,11 @@
 def fib():
-	''' all fibonacci'''
-	return('happy')
+    ''' all fibonacci'''
+    if N == 0 :
+        return 1
+    if N== 1 : 
+        return 1
+    return fib(N-1) + fib(N-2)
+
+if __name__=="__main":
+    for i in range(5):
+        print(fib(i))
